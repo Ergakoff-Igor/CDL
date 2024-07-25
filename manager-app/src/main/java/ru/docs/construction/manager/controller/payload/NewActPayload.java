69789720916ded71cb2172585a5b3c0dc055194b
Ierga <i.ergakoff@gmail.com>
2024-07-25 20:47:37 +0300

    CDL. Rest. Start

--- manager-app/src/main/java/ru/docs/construction/manager/controller/payload/NewActPayload.java
+++ manager-app/src/main/java/ru/docs/construction/manager/controller/payload/NewActPayload.java
@@ -7,14 +7,12 @@ import jakarta.validation.constraints.Size;
 
 public record NewActPayload(
         @NotEmpty(message = "{catalogue.acts.create.errors.month_is_null}")
-//        @NotEmpty(message = "{catalogue.products.create.errors.title_is_null}")
-//        @Size(min = 3, max = 50, message = "{catalogue.products.create.errors.title_size_is_invalid}")
         String month,
 
+        @NotNull(message = "{catalogue.acts.create.errors.year_is_null}")
         Short year,
 
         @NotEmpty(message = "{catalogue.acts.create.errors.section_is_null}")
-//        @Size(max = 1000, message = "{catalogue.products.create.errors.details_size_is_invalid}")
         String section,
 
         Double price) {
--- manager-app/src/main/java/ru/docs/construction/manager/controller/payload/UpdateActPayload.java
+++ manager-app/src/main/java/ru/docs/construction/manager/controller/payload/UpdateActPayload.java
@@ -7,6 +7,7 @@ public record UpdateActPayload(
         @NotEmpty(message = "{catalogue.acts.update.errors.month_is_null}")
         String month,
 
+        @NotNull(message = "{catalogue.acts.update.errors.year_is_null}")
         Short year,
 
         @NotEmpty(message = "{catalogue.acts.update.errors.section_is_null}")
