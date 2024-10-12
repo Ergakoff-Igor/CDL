3ab83d1f0e9ded40a8e7a8bbde8de8a0c241ee0f
Igor Ergakov <i.ergakoff@gmail.com>
2024-10-12 14:29:46 +0300

    CDL. OAuth2. MVC

---
+++ catalogue-service/src/main/java/ru/docs/construction/catalogue/config/SecurityBeans.java
@@ -0,0 +1,37 @@
+package ru.docs.construction.catalogue.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityBeans {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+                        .requestMatchers(HttpMethod.POST, "/catalogue-api/acts")
+                        .hasAuthority("SCOPE_edit_actslog")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/acts/{actId:\\d}")
+                        .hasAuthority("SCOPE_edit_actslog")
+                        .requestMatchers(HttpMethod.DELETE, "/catalogue-api/acts/{actId:\\d}")
+                        .hasAuthority("SCOPE_edit_actslog")
+                        .requestMatchers(HttpMethod.PATCH, "/catalogue-api/acts/{actId:\\d+}/{status}")
+                        .hasAuthority("SCOPE_edit_actslog")
+                        .requestMatchers(HttpMethod.GET)
+                        .hasAuthority("SCOPE_view_actslog")
+                        .anyRequest().denyAll())
+                .csrf(CsrfConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer(oauth2ResourceServer -> oauth2ResourceServer
+                        .jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
--- manager-app/src/main/java/ru/docs/construction/manager/config/ClientBeans.java
+++ manager-app/src/main/java/ru/docs/construction/manager/config/ClientBeans.java
@@ -1,5 +1,9 @@
 package ru.docs.construction.manager.config;
 
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import ru.docs.construction.manager.security.OAuthClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import ru.docs.construction.manager.client.RestClientActsRestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,9 +15,16 @@ public class ClientBeans {
 
     @Bean
     public RestClientActsRestClient actsRestClient(
-            @Value("${acts-log.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri) {
+            @Value("${acts-log.service.catalogue.uri:http://localhost:8081}") String catalogueBaseUri,
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository authorizedClientRepository,
+            @Value("${acts-log.services.catalogue.registration-id:keycloak}") String registrationId) {
         return new RestClientActsRestClient(RestClient.builder()
                 .baseUrl(catalogueBaseUri)
+                .requestInterceptor(
+                        new OAuthClientHttpRequestInterceptor(
+                                new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository,
+                                        authorizedClientRepository), registrationId))
                 .build());
     }
 }
